813f69fb27287a4b4d3e3368aeccfef963269186
Maja Bojarska <majabojarska98@gmail.com>
2020-03-02 11:24:36 +0100

    Implement pthread wrapper.

---
+++ src/Simulation/Thread.cpp
@@ -0,0 +1,36 @@
+#include "Thread.h"
+
+Thread::Thread() = default;
+
+Thread::~Thread() = default;
+
+unsigned int Thread::tid() const {
+    return 0;
+}
+
+void Thread::start(void *threadArg) {
+    int ret;
+    this->arg = threadArg;
+    /*
+     * Since pthread_create is a C library function, the 3rd argument is
+     * a global function that will be executed by the thread. In C++, we
+     * emulate the global function using the static member function that
+     * is called exec. The 4th argument is the actual argument passed to
+     * the function exec. Here we use this pointer, which is an instance
+     * of the Thread class.
+     */
+    if ((ret = pthread_create(&_id, nullptr, &Thread::exec, this)) != 0) {
+        throw "Error";
+    }
+    this->started = true;
+}
+
+void Thread::join() {
+    // Allow the thread to wait for the termination status
+    pthread_join(_id, nullptr);
+}
+
+void *Thread::exec(void *thr) {
+    // Function that is to be executed by the thread
+    reinterpret_cast<Thread *> (thr)->run();
+}
---
+++ src/Simulation/Thread.h
@@ -0,0 +1,41 @@
+//
+// Created by maja on 02.03.2020.
+//
+
+#ifndef SO2_THREAD_H
+#define SO2_THREAD_H
+
+#include "pthread.h"
+
+class Thread {
+
+private:
+    pthread_t _id;
+
+    // Prevent copying or assignment
+    Thread(const Thread &arg);
+
+    Thread &operator=(const Thread &rhs);
+
+protected:
+    bool started = false;
+    void *arg = nullptr;
+
+    static void *exec(void *thr);
+
+public:
+    Thread();
+
+    virtual ~Thread();
+
+    unsigned int tid() const;
+
+    void start(void *threadArg = nullptr);
+
+    void join();
+
+    virtual void run() = 0;
+};
+
+
+#endif //SO2_THREAD_H
