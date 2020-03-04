26324a2409e25d9446518bba94ad81c63f65a6b2
Maja Bojarska <majabojarska98@gmail.com>
2020-03-04 22:37:22 +0100

    Change stop names to something more meaningful

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,56 +5,57 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow),
       renderArea(new RenderArea(this)) {
   auto *world = new MPKWorld();
-  Stop *stop = new Stop(Vector2i(300, 300), "Kościuszki");
-  Stop *stop_another = new Stop(Vector2i(500, 400), "Komuny Paryskiej");
-  Stop *stop_another_other = new Stop(Vector2i(720, 300), "Plac Wróblewskiego");
-  Stop *stop_another_other_hehe =
+
+  Stop *stopKosciuszki = new Stop(Vector2i(300, 300), "Kościuszki");
+  Stop *stopKomunyParyskiej = new Stop(Vector2i(500, 400), "Komuny Paryskiej");
+  Stop *stopPlacWroblewskiego = new Stop(Vector2i(720, 300), "Plac Wróblewskiego");
+  Stop *stopMostGrunwaldzki =
       new Stop(Vector2i(820, 200), "Most Grunwaldzki");
-  Stop *stop_another_other_hehe_hihi =
+  Stop *stopPlacGrunwaldzki =
       new Stop(Vector2i(299, 100), "Plac Grunwaldzki");
-  Stop *stop_another_other_hehe_hihi_huhu =
+  Stop *stopNowowiejska =
       new Stop(Vector2i(800, 600), "Nowowiejska");
-  Stop *stop_another_other_hehe_hihi_huhu_haha =
+  Stop *stopPlacDominikanski =
       new Stop(Vector2i(200, 550), "Plac Dominikański");
-  world->stops.push_back(stop);
-  world->stops.push_back(stop_another);
-  world->stops.push_back(stop_another_other);
-  world->stops.push_back(stop_another_other_hehe);
-  world->stops.push_back(stop_another_other_hehe_hihi);
-  world->stops.push_back(stop_another_other_hehe_hihi_huhu);
-  world->stops.push_back(stop_another_other_hehe_hihi_huhu_haha);
+  world->stops.push_back(stopKosciuszki);
+  world->stops.push_back(stopKomunyParyskiej);
+  world->stops.push_back(stopPlacWroblewskiego);
+  world->stops.push_back(stopMostGrunwaldzki);
+  world->stops.push_back(stopPlacGrunwaldzki);
+  world->stops.push_back(stopNowowiejska);
+  world->stops.push_back(stopPlacDominikanski);
 
   auto *relation = new Relation("16");
-  relation->addStop(stop);
-  relation->addStop(stop_another);
-  relation->addStop(stop_another_other);
-  relation->addStop(stop_another_other_hehe);
-  relation->addStop(stop_another_other_hehe_hihi);
-  relation->addStop(stop, false); // cycle
+  relation->addStop(stopKosciuszki);
+  relation->addStop(stopKomunyParyskiej);
+  relation->addStop(stopPlacWroblewskiego);
+  relation->addStop(stopMostGrunwaldzki);
+  relation->addStop(stopPlacGrunwaldzki);
+  relation->addStop(stopKosciuszki, false); // cycle
 
   auto *relation2 = new Relation("OL", "blue");
-  relation2->addStop(stop);
-  relation2->addStop(stop_another);
-  relation2->addStop(stop_another_other);
-  relation2->addStop(stop, false);
+  relation2->addStop(stopKosciuszki);
+  relation2->addStop(stopKomunyParyskiej);
+  relation2->addStop(stopPlacWroblewskiego);
+  relation2->addStop(stopKosciuszki, false);
 
   auto *relation3 = new Relation("OP", "green");
-  relation3->addStop(stop_another);
-  relation3->addStop(stop_another_other);
-  relation3->addStop(stop_another, false);
+  relation3->addStop(stopKomunyParyskiej);
+  relation3->addStop(stopPlacWroblewskiego);
+  relation3->addStop(stopKomunyParyskiej, false);
 
   auto *relation4 = new Relation("5", "magenta");
-  relation4->addStop(stop_another);
-  relation4->addStop(stop_another_other);
-  relation4->addStop(stop_another_other_hehe_hihi_huhu);
-  relation4->addStop(stop_another, false);
+  relation4->addStop(stopKomunyParyskiej);
+  relation4->addStop(stopPlacWroblewskiego);
+  relation4->addStop(stopNowowiejska);
+  relation4->addStop(stopKomunyParyskiej, false);
 
   auto *relation5 = new Relation("17", "cyan");
-  relation5->addStop(stop_another);
-  relation5->addStop(stop_another_other);
-  relation5->addStop(stop_another_other_hehe_hihi_huhu);
-  relation5->addStop(stop_another_other_hehe_hihi_huhu_haha);
-  relation5->addStop(stop_another, false);
+  relation5->addStop(stopKomunyParyskiej);
+  relation5->addStop(stopPlacWroblewskiego);
+  relation5->addStop(stopNowowiejska);
+  relation5->addStop(stopPlacDominikanski);
+  relation5->addStop(stopKomunyParyskiej, false);
 
   world->relations.push_back(relation);
   world->relations.push_back(relation2);
@@ -71,6 +72,7 @@ MainWindow::MainWindow(QWidget *parent)
 
   renderArea->setMPKWorld(world);
   ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow() = default;
